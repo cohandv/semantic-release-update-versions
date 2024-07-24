c919ee7ebf04b9e49f873d1aef7ba20a119995d7
Juan Pablo Calvo <cjpablo92@gmail.com>
2024-07-24 18:15:20 -0300

    chore: added docker image push integration

--- src/localRun.ts
+++ src/localRun.ts
@@ -2,6 +2,12 @@
 import { AWS } from './aws.js';
 
 import { publish } from './publish.js';
+// import { prepare } from './prepare.js';
+import { Docker } from './docker.js'
+import { getError } from './error.js'
+import { stderr, stdout } from 'process';
+
+import type { ReleaseType } from 'semantic-release'
 
 async function run() {
     const awsConfig = {
@@ -9,19 +15,82 @@ async function run() {
     }
 
     const aws = new AWS(awsConfig.region, null, null);
-    console.log(aws.awsEcr);
     const awsLoginValue = await aws.login()
 
-    console.log(`Successfully logged in to ${awsLoginValue.registry}`)
+    console.log(`Successfully logged in to ${awsLoginValue.registry}`);
+
+    const docker = new Docker()
+    const dockerLogin = await docker.login(awsLoginValue.username, awsLoginValue.password, awsLoginValue.registry)
+
+    if (!dockerLogin) {
+        throw new AggregateError([getError('ENOAUTHENTICATION')])
+    }
+
+    if(dockerLogin) {
+        console.log('Docker login successful');
+    }
 
     const pluginConfig = {
-        "buildImage": "docker build . -f ../Dockerfile",
+        "buildImage": "docker build . -f ../Dockerfile -t my-ecr-image",
         "imageName": "my-ecr-image",
-        "tags": ["latest"],
+        "tags": ["latest", "1.0.0"],
         "bumpParents": false
     }
 
-    // publish(pluginConfig, );
+    const context = {
+        nextRelease: {
+            type: "major" as ReleaseType,
+            channel: "latest",
+            version: "1.0.0",
+            gitHead: "1234567890",
+            gitTag: "v0.0.0",
+            name: "v0.0.0"
+        },
+        env: {},
+        commits: [],
+        releases: [],
+        lastRelease: {
+            version: "0.0.0",
+            gitHead: "1234567890",
+            gitTag: "v0.0.0",
+            channel: "latest",
+            name: "v0.0.0",
+            channels: []
+        },
+        envCi: {
+            isCi: false,
+            commit: "1234567890",
+            branch:"main" ,
+        },
+        branch: { 
+            name:"main" 
+        },
+        options: {},
+        branches: [],
+        logger: console,
+        stdout: stdout,
+        stderr: stderr
+
+    }
+
+    const dockerConfig = Docker.loadConfig(pluginConfig, context)
+
+    // await prepare(pluginConfig, context);
+    context.logger.log(
+        `Pushing ${pluginConfig.imageName} with tags [${dockerConfig.imageTags.join(', ')}] to ${awsLoginValue.registry}`,
+    )
+
+    const dockerPush = await docker.push(dockerConfig.imageName, dockerConfig.imageTags, awsLoginValue.registry)
+
+    if (!dockerPush) {
+        throw new AggregateError([getError('EDEPLOY')])
+    }
+
+    context.logger.log(
+        `Successfully pushed ${pluginConfig.imageName} with tags [${dockerConfig.imageTags.join(', ')}] to ${awsLoginValue.registry}`,
+    )
+
+    // publish(pluginConfig, context);
 }
 
 run();
